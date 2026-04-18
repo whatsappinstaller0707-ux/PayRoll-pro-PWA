925a15050a09c1ce5710f6a1bd96da7742c8ee75
whatsappinstaller0707-ux <whatsappinstaller0707@gmail.com>
2026-04-18 12:54:11 +0200

    Upgrade CACHE_NAME to 'payroll-v2' and add cache cleanup
    Updated cache version and added activation event to clean up old caches.

--- sw.js
+++ sw.js
@@ -1,15 +1,28 @@
-const CACHE_NAME = 'payroll-v1';
+const CACHE_NAME = 'payroll-v2';
 const ASSETS = [
+  './',
   'index.html',
   'style.css',
   'script.js',
   'manifest.json',
+  'icon.png',
   'https://cdn.sheetjs.com/xlsx-0.20.1/package/dist/xlsx.full.min.js'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
